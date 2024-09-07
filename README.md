fed7e67a958fe6eef710eed3abc03d0363ae1a1e
jutechs <jutechs@jutechs.de>
2024-09-07 19:07:02 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# signItem
+# signItem
---
+++ src/main/java/de/jutechs/signitem/Main.java
@@ -0,0 +1,10 @@
+package de.jutechs.signitem;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
